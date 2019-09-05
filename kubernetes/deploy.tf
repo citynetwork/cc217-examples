8bd3c19c1f366836797a518631d7447e4e83e059
Florian Haas <florian@citynetwork.eu>
2019-09-05 15:17:24 +0200

    Simplify Kubernetes configuration

--- kubernetes/deploy.tf
+++ kubernetes/deploy.tf
@@ -21,22 +21,11 @@ resource "kubernetes_deployment" "nginx" {
       }
       spec {
         container {
-          image = "nginx:1.7.8"
+          image = "nginx"
           name  = "example"
 
           port {
             container_port = 80
-          }
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
           }
         }
       }
