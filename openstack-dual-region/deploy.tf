8a1087477d533611b3944aa536234c957b079459
Christos Varelas <christos.varelas@cleura.com>
2025-01-08 16:16:32 +0200

    fix: Remove stray closing curly bracket
    There is a stray closing curly bracket in
    openstack-dual-region/deploy.tf, which causes
    tofu to abort applying the configuration.

--- openstack-dual-region/deploy.tf
+++ openstack-dual-region/deploy.tf
@@ -73,8 +73,6 @@ resource "openstack_compute_instance_v2" "instance" {
   }
 }
 
-}
-
 resource "openstack_compute_floatingip_associate_v2" "floatingip" {
   floating_ip = "${openstack_networking_floatingip_v2.floatingip.address}"
   instance_id = "${openstack_compute_instance_v2.instance.id}"
